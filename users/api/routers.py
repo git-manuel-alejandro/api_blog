476d3e600e631afa2f6945f582e7fe9f37687417
Manuel Roa <manuel.roaojeda23@gmail.com>
2022-02-11 16:30:13 -0300

    login with jwt

--- users/api/routers.py
+++ users/api/routers.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from users.api.views import RegisterView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 
 urlpatterns = [
-    path('auth/register/', RegisterView.as_view())
+    path('auth/register/', RegisterView.as_view()),
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
