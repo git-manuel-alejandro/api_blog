6d2a725a672cf966eed7822a9e9155feefc579cc
Manuel Roa <manuel.roaojeda23@gmail.com>
2022-02-12 13:48:22 -0300

    lookup_field = 'slug'

--- categories/api/views.py
+++ categories/api/views.py
@@ -1,3 +1,4 @@
+from cgitb import lookup
 from rest_framework.viewsets import ModelViewSet
 from categories.models import Category
 from categories.api.serializers import CategorySerializer
@@ -8,3 +9,4 @@ class CategoryApiViewSet(ModelViewSet):
     permission_classes = [IsAdminOrReadOnly]
     serializer_class = CategorySerializer
     queryset = Category.objects.all()
+    lookup_field = 'slug'
