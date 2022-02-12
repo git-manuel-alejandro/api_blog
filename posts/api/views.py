19d32ee8e770c12d353c12b425a9ab1b36709a83
Manuel Roa <manuel.roaojeda23@gmail.com>
2022-02-12 20:35:03 -0300

    setting permissions in post

--- posts/api/views.py
+++ posts/api/views.py
@@ -1,9 +1,11 @@
 from rest_framework.viewsets import ModelViewSet
 from posts.api.serializers import PostSerializer
 from posts.models import Post
+from posts.api.permissions import IsAdminOrReadOnly
 
 
 class PostView(ModelViewSet):
+    permission_classes = [IsAdminOrReadOnly]
     serializer_class = PostSerializer
     queryset = Post.objects.filter(published=False)
     lookup_field = 'slug'
