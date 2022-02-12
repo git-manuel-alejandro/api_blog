02d9e6cf59a0365631ac3e334b6468dbf7f70522
Manuel Roa <manuel.roaojeda23@gmail.com>
2022-02-12 14:02:24 -0300

    adding filters

--- categories/api/views.py
+++ categories/api/views.py
@@ -3,10 +3,14 @@ from rest_framework.viewsets import ModelViewSet
 from categories.models import Category
 from categories.api.serializers import CategorySerializer
 from categories.api.permissions import IsAdminOrReadOnly
+from django_filters.rest_framework import DjangoFilterBackend
 
 
 class CategoryApiViewSet(ModelViewSet):
     permission_classes = [IsAdminOrReadOnly]
     serializer_class = CategorySerializer
-    queryset = Category.objects.all()
+    # queryset = Category.objects.all()
+    queryset = Category.objects.filter(published=True)
     lookup_field = 'slug'
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['title']
