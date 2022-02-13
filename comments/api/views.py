830f1e4dcd615174a23b388de856ceada5da0a63
Manuel Roa <manuel.roaojeda23@gmail.com>
2022-02-13 07:03:39 -0300

    ordering filter by date time created

--- comments/api/views.py
+++ comments/api/views.py
@@ -1,4 +1,5 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.filters import OrderingFilter
 from comments.api.serializers import CommentSerializer
 from comments.models import Commet
 
@@ -6,3 +7,5 @@ from comments.models import Commet
 class CommentsView(ModelViewSet):
     serializer_class = CommentSerializer
     queryset = Commet.objects.all()
+    filter_backends = [OrderingFilter]
+    ordering = ['-created_at']
