37e7580bc858de0505509f841840abd10b384661
Manuel Roa <manuel.roaojeda23@gmail.com>
2022-02-13 06:36:57 -0300

    comment model

--- comments/admin.py
+++ comments/admin.py
@@ -1,3 +1,12 @@
+from xml.dom.minidom import Comment
 from django.contrib import admin
+from comments.models import Commet
 
 # Register your models here.
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['content', 'user', 'post', 'created_at']
+
+
+admin.site.register(Commet)
--- comments/models.py
+++ comments/models.py
@@ -1,3 +1,14 @@
+from re import U
 from django.db import models
+from users.models import User
+from posts.models import Post
 
-# Create your models here.
+
+class Commet(models.Model):
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return self.content
