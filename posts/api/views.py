ce86ee6ebfb80a6c774d05e3c1b84087aac50d47
Manuel Roa <manuel.roaojeda23@gmail.com>
2022-02-13 06:21:00 -0300

    adding data of user and category

--- posts/api/views.py
+++ posts/api/views.py
@@ -1,11 +1,16 @@
 from rest_framework.viewsets import ModelViewSet
+from django_filters.rest_framework import DjangoFilterBackend
 from posts.api.serializers import PostSerializer
-from posts.models import Post
 from posts.api.permissions import IsAdminOrReadOnly
+from posts.models import Post
 
 
 class PostView(ModelViewSet):
     permission_classes = [IsAdminOrReadOnly]
     serializer_class = PostSerializer
-    queryset = Post.objects.filter(published=False)
+    queryset = Post.objects.all()
+    # queryset = Post.objects.filter(published=False)
     lookup_field = 'slug'
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['category', 'category__slug']
+    # filterset_fields = ['category']
--- users/api/serializers.py
+++ users/api/serializers.py
@@ -1,3 +1,4 @@
+from dataclasses import fields
 from rest_framework import serializers
 from users.models import User
 
@@ -21,6 +22,7 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'email', 'username', 'first_name', 'last_name']
+        # fields = ['email']
 
 
 class UserUpdateSerializer(serializers.ModelSerializer):
